105ff53de6a8a88f1b559ba98282814d707fe85a
chaSenecHo <101160308+chaSenecHo@users.noreply.github.com>
2022-10-17 14:10:12 +0100

    verilog code

--- Stump/Stump_ALU/functional/verilog.v
+++ Stump/Stump_ALU/functional/verilog.v
@@ -32,7 +32,84 @@ module Stump_ALU (input  wire [15:0] operand_A,		// First operand
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 /* Verilog code                                                               */
-
+always@(*)begin
+	case (func)
+		3'b000: //add
+			begin
+				result = operand_A + operand_B;
+				flags_out[3] = result[15];
+				if (result == 0)
+					flags_out[2] = 1'b1;
+				if (operand_A[15] == operand_B[15] && operand_A[15] != result[15])
+					flags_out[1] = 1'b1;
+				flags_out[0] = result[16];
+			end
+		3'b001: //adc
+			begin
+				result = operand_A + operand_B + c_in;
+				flags_out[3] = result[15];
+				if (result == 0)
+					flags_out[2] = 1'b1;
+				if (operand_A[15] == operand_B[15] && operand_A[15] != result[15])
+					flags_out[1] = 1'b1;
+				flags_out[0] = result[16];
+			end
+		3'b010: //sub
+			begin
+				result = operand_A - operand_B;
+				flags_out[3] = result[15];
+				if (result == 0)
+					flags_out[2] = 1'b1;
+				if (operand_A[15] != operand_B[15] && operand_B[15] == result[15])
+					flags_out[1] = 1'b1;
+				flags_out[0] = ~result[16];
+			end
+		3'b011: //sbc
+			begin
+				result = operand_A - operand_B - c_in;
+				flags_out[3] = result[15];
+				if (result == 0)
+					flags_out[2] = 1'b1;
+				if (operand_A[15] != operand_B[15] && operand_B[15] == result[15])
+					flags_out[1] = 1'b1;
+				flags_out[0] = ~result[16];
+			end
+		3'b100: //and
+			begin
+				result = operand_A & operand_B;
+				flags_out[3] = result[15];
+				if (result == 0)
+					flags_out[2] = 1'b1;
+				flags_out[1] = 1'b0;
+				flags_out[0] = csh;
+			end
+		3'b101: //or
+			begin
+				result = operand_A | operand_B;
+				flags_out[3] = result[15];
+				if (result == 0)
+					flags_out[2] = 1'b1;
+				flags_out[1] = 1'b0;
+				flags_out[0] = csh;
+			end
+		3'b110: //ldst
+			begin
+				result = operand_B;
+				flags_out[3] = 1'bx;
+				flags_out[2] = 1'bx;
+				flags_out[1] = 1'bx;
+				flags_out[0] = 1'bx;
+			end
+		3'b111: //bcc
+			begin
+				result = operand_A;
+				flags_out[3] = 1'bx;
+				flags_out[2] = 1'bx;
+				flags_out[1] = 1'bx;
+				flags_out[0] = 1'bx;
+			end
+	endcase
+end
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
